60466f07d2350c7f6fdbd6c265070518b6ffb266
Nicolaas <github@sunnysideup.co.nz>
2020-02-10 13:10:20 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ git clone https://github.com/rapidwebltd/php-switch-scripts.git
 cd php-switch-scripts/
 bash setup.sh
 cd ..
-mv php-switch-scripts/ /usr/bin/local/
-chmod +x /usr/local/bin/php-switch
+sudo mv php-switch-scripts /usr/local/bin/
+sudo chmod +x /usr/local/bin/php-switch
 echo "you can now use php-switch 7.1  to switch to any version of PHP"
