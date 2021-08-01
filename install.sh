aee7025b0f1dce15b6fa3c183d885494e0782dd6
Nicolaas <github@sunnysideup.co.nz>
2021-08-01 20:49:26 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ git clone https://github.com/rapidwebltd/php-switch-scripts.git
 cd php-switch-scripts/
 bash setup.sh
 cd ..
-mv php-switch-scripts /usr/local/bin/
-cat <<EOT >> /usr/local/bin/php-switch
+sudo mv php-switch-scripts /usr/local/bin/
+sudo cat <<EOT >> /usr/local/bin/php-switch
 if [ \$# -eq 0 ]
   then
     echo "Please supply the php version you would like to switch to as an argument."
@@ -12,5 +12,5 @@ else
    bash /usr/local/bin/php-switch-scripts/switch-to-php-\$1.sh
 fi
 EOT
-chmod +x /usr/local/bin/php-switch
+sudo chmod +x /usr/local/bin/php-switch
 echo "you can now use php-switch 7.1  to switch to any version of PHP"
