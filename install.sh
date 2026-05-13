34918787134521005eef9af0fab6e4a90d372f27
Sunny Side Up <github@sunnysideup.co.nz>
2026-05-14 08:04:09 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,5 @@ git clone https://github.com/sunnysideup/php-switch-scripts.git
 cd php-switch-scripts/
 bash setup.sh
 cd ..
-mv php-switch-scripts /usr/local/bin/
-cat <<EOT >> /usr/local/bin/php-switch
-if [ \$# -eq 0 ]
-  then
-    echo "Please supply the php version you would like to switch to as an argument."
-else
-   bash /usr/local/bin/php-switch-scripts/switch-to-php-\$1.sh
-fi
-EOT
-chmod +x /usr/local/bin/php-switch
-echo "you can now use php-switch 7.1  to switch to any version of PHP"
+mv php-switch-scripts/php-switch /usr/local/bin/
+
